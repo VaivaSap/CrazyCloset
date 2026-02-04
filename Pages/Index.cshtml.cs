ddeeac0f3f914b67f01c48b6ee781eca185dfaff
VaivaSap <vaiva.sapetkaite@gmail.com>
2026-02-04 15:20:23 +0200

    Pictures' carousel displays images and I can change them with previous/next buttons; still need to solve edge case and wrap back the queque

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,4 +1,3 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace CrazyCloset.Pages
@@ -7,6 +6,8 @@ namespace CrazyCloset.Pages
     {
         private readonly ILogger<IndexModel> _logger;
 
+        public List<string> ClothingPictures { get; set; }
+
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
@@ -14,7 +15,10 @@ namespace CrazyCloset.Pages
 
         public void OnGet()
         {
+            var path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            var picsPath = Path.Combine(path, "crazycloset_items", "Items");
 
+            ClothingPictures = Directory.GetFiles(picsPath).Select(f => Path.GetFileName(f)).ToList();
         }
     }
 }
