62fd76798aabf8cd23aca74b8efad4d4deddaa0d
VaivaSap <vaiva.sapetkaite@gmail.com>
2026-03-02 13:32:16 +0200

    Heatmap works for concrete items

---
+++ Pages/ItemHeatmap.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using CrazyCloset.Models;
+using CrazyCloset.Services;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CrazyCloset.Pages
+{
+    public class ItemHeatmap : PageModel
+    {
+        private readonly IInventoryService _inventoryService;
+
+        public List<UseLogDto> ItemLogs { get; set; } = new List<UseLogDto>();
+        public List<ClothesItem> ClothesItems { get; set; } = new List<ClothesItem>();
+
+
+        public ItemHeatmap(IInventoryService inventoryService)
+        {
+            _inventoryService = inventoryService;
+        }
+
+        public async Task OnGetAsync(long id)
+        {
+            ItemLogs = await _inventoryService.GetAllLogsByIdAsync(id); 
+        }
+    }
+}
